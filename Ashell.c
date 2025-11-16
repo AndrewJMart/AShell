546c4b58455b6b81d4eb9f45d0f5238b36ab04aa
AndrewJMart <amart89531@gmail.com>
2025-11-15 18:31:11 -0800

    Add: Take 1 Line Of Input

--- Ashell.c
+++ Ashell.c
@@ -2,7 +2,19 @@
 #include <stdlib.h>
 
 int main() {
-    printf("Hello AShell!\n");
+    printf("\nHello I'm AShell!\n");
 
+    // Test: Try Taking One Line Of User Input
+    // Use Getline
+    char *input_buffer;
+    size_t buffer_size = 5;
+
+    input_buffer = (char *)malloc(sizeof(char) * buffer_size);
+
+    // Collect Line From User
+    getline(&input_buffer, &buffer_size, stdin);
+
+    printf("You Typed: %s\n", input_buffer);
+    
     return 0;
 }
