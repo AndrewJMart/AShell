90c9a1dd0e4336164ce57e76a86e8b2feaca4223
AndrewJMart <amart89531@gmail.com>
2025-11-15 19:13:54 -0800

    Add: Simple Single Line Parsing

--- Ashell.c
+++ Ashell.c
@@ -1,5 +1,6 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     printf("\nHello I'm AShell!\n");
@@ -9,16 +10,26 @@ int main() {
     
     while(1) {
     
-        char *input_buffer;
         size_t buffer_size = 5;
 
-        input_buffer = (char *)malloc(sizeof(char) * buffer_size);
+        char *input_buffer = (char *)malloc(sizeof(char) * buffer_size);
 
         // Collect Line From User
         getline(&input_buffer, &buffer_size, stdin);
 
         printf("You Typed: %s\n", input_buffer);
+
+        // Now Parse Input Line For First Token
         
+        // Ensure Input Buffer Still Points To Beginning Of String
+        char *input_pointer = input_buffer;
+
+        char *space_delim = " ";
+        char *next_token = strsep(&input_pointer, space_delim);
+
+        printf("Here Is First Token Found, %s\n", next_token);
+        printf("Here Is Moved Past String, %s\n", input_pointer);
+
         free(input_buffer);
     }
     
